449fb5bb6bbf0889e391cbfe92802523bbd0aab0
munasing <madhusankaashan41@gmail.com>
2019-03-06 14:54:50 +0530

    this is handler

--- app/src/main/java/com/example/countdowntime/MainActivity.java
+++ app/src/main/java/com/example/countdowntime/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.countdowntime;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void countDown(){
+        final TextView textview = findViewById(R.id.textView);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textview.setText(Integer.toString(conter));
+                counter--;
+                handler.postDelayed(r:this, delayMills:1000);
+            }
+        })
     }
 }
